56b4f6a0a6c81c2f40dff589ada7569fd08418d0
setya7 <christyastama@gmail.com>
2016-03-29 10:13:58 +0700

    diupdate nih

--- cb.java
+++ cb.java
@@ -12,33 +12,33 @@ package pkg2;
 public class cb {
     public static void main(String[]args){
 System.out.println("Tamatam");
-System.out.println("");
+System.out.println("0");
 int l = 1;
 int tingkat = 10;
 int n = 12/2;
 for(int i=1;i<=n;i++){
 for(int k=n;k>i;k--){
-System.out.print(" ");
+System.out.print("0");
 }
 for(int j=1;j<=l;j++){
 System.out.print("+");
 }
 l+=2;
 
-System.out.println(" ");
+System.out.println("0");
 }
 l -= 4;
 
 for(int i=(n-1);i>=1;i--){
 for(int k=i;k<=(n-1);k++){
-System.out.print(" ");
+System.out.print("0");
 }
 for(int j=l;j>=1;j--){
 System.out.print("+");
 }
 l-=2;
 
-System.out.println("");
+System.out.println("0");
 }
 
 }
